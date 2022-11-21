28f6458e1498cd43f28b3bd5e4513230c24092ff
Prerana <dahiwadeprerana@gmail.com>
2022-11-21 20:39:09 +0530

    [prerana]Add:PushElementInStack

---
+++ StackAndQueueDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StacksAndQueueDemo
+{
+    public  class Program
+    {
+        public static void Main(string[]args)
+        {
+            StacksLinkedList stacksLinkedList = new StacksLinkedList();
+            stacksLinkedList.Push(56);
+            stacksLinkedList.Push(30);
+            stacksLinkedList.Push(70);
+            stacksLinkedList.Display();
+        }
+
+    }
+}
---
+++ StackAndQueueDemo/StackLinkedList.cs
@@ -0,0 +1,42 @@
+﻿using StackAndQueueDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StacksAndQueueDemo
+{
+    public class StacksLinkedList
+    {
+        private Node top;
+        public void  Stack()
+        {
+            this.top = null;
+        }
+        public void Push(int value) // add
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine("After Pushed Element :" +temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
